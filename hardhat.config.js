56b92c492c3af1ff533d8d9cb5cc2ea29c717f18
Darren Jensen <darren.jensen@gmail.com>
2023-08-30 14:04:01 +0700

    fix hardhat config network interface

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,64 +33,21 @@ require("dotenv").config();
           "nick lucian brenda kevin sam fiscal patch fly damp ocean produce wish",
         count: 40,
       },
-      // forking: {
-      //   url: "https://eth-mainnet.alchemyapi.io/v2/7dW8KCqWwKa1vdaitq-SxmKfxWZ4yPG6"
-      // },
       allowUnlimitedContractSize: true
     },
-    // goerli: {
-    //      url: `${process.env.NODE_URL_GOERLI}`,
-    //      seeds: [process.env.PRIVATE_KEY],
-    //      gas: 10000000 ,
-    //      gasPrice: 40000000000
-    // }// ,
-    // mainnet: {
-    //      url: `${process.env.NODE_URL_MAINNET}`,
-    //      seeds: [process.env.PRIVATE_KEY],
-    //      gas: 3000000 ,
-    //      gasPrice: 300000000000
-    //    },
-    // mumbai: {
-    //     url: `${process.env.NODE_URL_MUMBAI}`,
-    //     accounts: [process.env.PRIVATE_KEY],
-    //     gas: 5000000 ,
-    //     gasPrice: 50000000000
-    //   }
-    // polygon: {
-    //   url: `${process.env.NODE_URL_MATIC}`,
-    //   seeds: [process.env.PRIVATE_KEY],
-    //   gas: 2000000 ,
-    //   gasPrice: 250000000000
-    // }
-    harmony_testnet: {
-      url: `${process.env.NODE_URL_HARMONY_TESTNET}`,
-      seeds: [process.env.PRIVATE_KEY],
-      gas: 2000000 ,
-      gasPrice: 250000000000
-    }
-    // arbitrum_testnet: {
-    //   url: `${process.env.NODE_URL_ARBITRUM_TESTNET}`,
-    //   seeds: [process.env.PRIVATE_KEY],
-    //   gas: 2000000 ,
-    //   gasPrice: 250000000000
-    // }
-    // optimism_testnet: {
-    //   url: `${process.env.NODE_URL_OPTIMISM_TESTNET}`,
-    //   seeds: [process.env.PRIVATE_KEY],
-    //   gas: 2000000 ,
-    //   gasPrice: 250000000000
-    // }
-    ,  pulse_testnet: {
+    pulse_testnet: {
       chainId: parseInt(process.env.CHAIN_ID_PULSECHAIN_TESTNET),
       url: `${process.env.NODE_URL_PULSECHAIN_TESTNET}`,
       seeds: [process.env.PRIVATE_KEY],
-      gasPrice: process.env.GAS_PRICE || 5e10
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: parseInt(process.env.GAS_PRICE) || 5e10
     },
     pulse_mainnet: {
       chainId: parseInt(process.env.CHAIN_ID_PULSECHAIN_MAINNET),
       url: `${process.env.NODE_URL_PULSECHAIN_MAINNET}`,
       seeds: [process.env.PRIVATE_KEY],
-      gasPrice: process.env.GAS_PRICE || 5e10
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: parseInt(process.env.GAS_PRICE) || 5e10
     }
   },
   etherscan: {
@@ -114,5 +71,4 @@ require("dotenv").config();
   gasReporter: {
     enabled: true
   }
-
 }
